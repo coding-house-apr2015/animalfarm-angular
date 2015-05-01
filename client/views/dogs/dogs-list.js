05d6dc04c66c445e120595f2bae2c8a26d58a734
Chyld Medford <chyld.medford@gmail.com>
2015-05-01 15:56:28 -0700

    delete

--- client/views/dogs/dogs-list.js
+++ client/views/dogs/dogs-list.js
@@ -1,5 +1,18 @@
 'use strict';
 
 angular.module('animalfarm')
-.controller('DogsListCtrl', function(){
+.controller('DogsListCtrl', function($scope, Dog){
+  Dog.find()
+  .then(function(response){
+    $scope.dogs = response.data.dogs;
+  });
+
+  $scope.destroy = function(obj){
+    Dog.destroy(obj._id)
+    .then(function(response){
+      _.remove($scope.dogs, function(dog){
+        return dog._id === response.data._id;
+      });
+    });
+  };
 });
--- client/views/dogs/dogs-show.js
+++ client/views/dogs/dogs-show.js
@@ -1,5 +1,9 @@
 'use strict';
 
 angular.module('animalfarm')
-.controller('DogsShowCtrl', function(){
+.controller('DogsShowCtrl', function($scope, $state, Dog){
+  Dog.findById($state.params.dogId)
+  .then(function(response){
+    $scope.dog = response.data;
+  });
 });
